bc341a2866ceecc2419edd57528370f48a64571e
H. Dale McBane <h.dale.mcbane@gmail.com>
2022-02-17 10:56:08 -0500

    we've moved changes

--- extract_foundtryvtt.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-if [ $# -eq 0 ]; then
-  echo "Usage: $(basename $0) <foundryvtt linux/node zip filename>"
-else
-  unzip "$1" -d "${HOME}/docker/foundryvtt/build/foundryvtt/latest/"
-fi
